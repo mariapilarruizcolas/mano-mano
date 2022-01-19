4f6fb3f9896c4b5a9014e7e02af690a6945c8034
MariaPilarRuizColas <mariapilar.ruizcolas@neuf.fr>
2022-01-19 21:37:01 +0100

    InitialCommit

--- src/components/Projets_cle_en_main.jsx
+++ src/components/Projets_cle_en_main.jsx
@@ -5,7 +5,12 @@ import React from 'react';
 
 function Projets_cle_en_main(){
   return(
+    <div>
     <h1>Projets cle en main</h1>
+    <h2>Cuisine</h2>
+    <h2>Salle de bain</h2>
+    <h2>Amenagement du jardin</h2>
+    </div>
   )
 }
 export  default Projets_cle_en_main;
